3fd53422ee7458bdd5959538890486036824d7c5
swapnil.sonawane <swapnil.sonawane@coditas.com>
2023-05-14 16:59:07 +0530

    Added Security Implementation

--- src/main/java/com/sd/controller/AuthenticationController.java
+++ src/main/java/com/sd/controller/AuthenticationController.java
@@ -6,9 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +24,6 @@ public class AuthenticationController {
     }
     @PostMapping("/authenticate")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest){
-        int i =0;
-        log.info("Test");
         AuthResponse authResponse = authenticationService.authenticate(authenticationRequest);
         return new ResponseEntity<>(authResponse, HttpStatus.OK);
     }
